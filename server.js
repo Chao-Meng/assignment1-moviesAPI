0607b0fe5c1206db32a517d2699d59af7def8bd4
Chao Meng <cmeng14@myseneca.ca>
2022-09-15 23:35:38 -0400

    4th commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 *  (including web sites) or distributed to other students.
 * 
 *  Name: __Chao Meng_____ Student ID: _128438215_ Date: __2022-09-15____
-*  Cyclic Link: _______________________________________________________________
+*  Cyclic Link: 
 *
 ********************************************************************************/ 
 
@@ -17,7 +17,7 @@ const app = express();
 const cors=require("cors");
 //require dotenv package to enable read code from .env file
 require('dotenv').config();
-const {MONGODB_CONN_STRING} = process.env;
+//const {MONGODB_CONN_STRING} = process.env;
 app.use(bodyParser.json());
 //declared to use cors
 app.use(cors());
@@ -28,7 +28,6 @@ const MoviesDB = require("./modules/moviesDB.js");
 const db = new MoviesDB();
 const HTTP_PORT = process.env.PORT || 8080;
 //const myUdatedConnectionString = MoviesDB("mongodb+srv://cmeng14:dE355PKsXXSAkwv@cluster0.wjwfi.mongodb.net/sample_mflix?retryWrites=true&w=majority");
-
 //MONGODB_CONN_STRING=myUdatedConnectionString;
 db.initialize(process.env.MONGODB_CONN_STRING).then(()=>{
     app.listen(HTTP_PORT, ()=>{
@@ -39,7 +38,7 @@ db.initialize(process.env.MONGODB_CONN_STRING).then(()=>{
 });
 
 //Add routs
-//GET/api/movies
+//GET/api/movies. must accept the numeric query parameters "page" and "perPage" 
 app.get("/api/movies",(req,res)=>{
     db.getAllMovies(req.query.page,req.query.perPage,req.query.title).then((msg)=>{
         console.log(msg);
@@ -49,9 +48,10 @@ app.get("/api/movies",(req,res)=>{
             res.status(200).json(msg);
         }  
     }).catch(err=>{
-        res.status(400).json({message:"err"});
+        res.status(400).json({message:err+" or please input your query detail, page , prePage and others"});
     });
 });
+
 
 //POST /api/movies
 app.post("/api/movies",(req,res)=>{
@@ -69,9 +69,10 @@ app.get("/api/movies/:id",(req,res)=>{
     db.getMovieById(req.params.id).then(movie=>{
         res.status(200).json(movie)
     }).catch(err=>{
-        res.status(400).json({message:err});
+        res.status(400).json({message:err + "please input the right query id info"});
     });
 });
+
 //PUT/api/movie
 app.put("/api/movies:id",(req,res)=>{
     myUdatedConnectionString.getMovieById(req.params.id).then((addMovie)=>{
@@ -86,6 +87,7 @@ app.put("/api/movies:id",(req,res)=>{
         res.json({message:err});
     });
 });
+
 //DETELE/api/movies
 app.delete("api/movies/:id",(req,res)=>{
     db.deleteMovieById(req.params.id).then((msg)=>{
@@ -95,6 +97,7 @@ app.delete("api/movies/:id",(req,res)=>{
     res.status(404).json({message:err});
     });
 })
+
 //single GET route "/" is required which returns the following object (JSON): {message: "API Listening"}
 app.get('/',(req,res)=>{
     res.json({message:"API Listening"});
